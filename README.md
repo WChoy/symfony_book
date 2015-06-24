63b7c2f61ff880390a45eae0182b34d707a18606
William Choy <wchoy@phase2technology.com>
2015-06-24 13:31:43 -0400

    Initial standard Symfony + Chapter.3

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony_book
+============
+
+A Symfony project created on June 23, 2015, 3:42 pm.
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,73 @@
+<?php
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+// Converting from a PSR-7 object to HttpFoundation symfony/psr-http-message-bridge
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+
+class LuckyController extends Controller
+{
+//  /**
+//   * @Route("/lucky/number")
+//   */
+//  public function numberAction()
+//  {
+//    $number = rand(0, 100);
+//    return new Response(
+//      '<html><body>Lucky number: '.$number.'</body></html>'
+//    );
+//  }
+
+  /**
+   * @Route("/api/lucky/number")
+   */
+  public function apiNumberAction()
+  {
+    $data = array(
+      'lucky_number' => rand(0, 100),
+    );
+    return new Response(
+      json_encode($data),
+      200,
+      array('Content-Type' => 'application/json')
+    );
+  }
+
+  /**
+   * @Route("/api/lucky/number2")
+   */
+  public function apiNumberAction2()
+  {
+    $data = array(
+      'lucky_number' => rand(0, 100),
+    );
+    // calls json_encode and sets the Content-Type header
+    return new JsonResponse($data);
+  }
+
+  /**
+   * @Route("/lucky/number/{count}")
+   */
+  public function numberAction($count)
+  {
+    $numbers = array();
+    for ($i = 0; $i < $count; $i++) {
+      $numbers[] = rand(0, 100);
+    }
+    $numbersList = implode(', ', $numbers);
+//    $html = $this->container->get('templating')->render(
+//      'lucky/number.html.twig',
+//      array('luckyNumberList' => $numbersList)
+//    );
+    $html = $this->render(
+      'lucky/number.html.twig',
+      array('luckyNumberList' => $numbersList)
+    );
+    return new Response($html);
+  }
+
+} 
